3df0711947380e9ee23c73921e38c56d8c03fb5d
nlezamaochoa <73286808+nlezamaochoa@users.noreply.github.com>
2020-11-18 15:04:24 +0100

    add package

---
+++ MyNewPackage/R/hello.R
@@ -0,0 +1,10 @@
+#' Hello World!
+#'
+#' Prints a the classic first program greeting. It takes no arguments.
+#'
+#' @examples
+#' hello()
+#' @export
+hello <- function() {
+  print("Hello, world!")
+}
